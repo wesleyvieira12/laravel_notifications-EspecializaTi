8afaeab0aa9b91cd721996588a25bced2a7cae4f
Wesley Vieira <wesleyvieira12@gmail.com>
2019-02-22 11:39:57 -0300

    Definindo os models e autenticação

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+
+	protected $fillable = [
+		'user_id',
+		'title',
+		'body'
+	];
+    public function comments()
+    {
+    	return $this->hasMany(Comment::class);
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
 }
